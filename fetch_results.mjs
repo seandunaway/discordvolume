703e6a6a5f23d194a33491795ede6b9ba490d9ff
seandunaway <git@seandunaway.com>
2024-02-07 17:48:38 -0800

    fetch results

---
+++ fetch_results.mjs
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+import {env} from 'node:process'
+import {writeFileSync} from 'node:fs'
+
+let guild = '519749471102304258'
+let channel = '849455257472991243'
+let date_start = new Date('6/1/2021')
+let date_stop = new Date('6/3/2021')
+
+if (!env.auth) throw new Error('auth')
+
+let data = {}
+let d = new Date(date_start)
+while (d <= date_stop) {
+	let d_next = new Date(d)
+	d_next.setDate(d.getDate() + 1)
+
+	let min_id = date_to_snowflake(d)
+	let max_id = date_to_snowflake(d_next)
+
+	let url = `https://discord.com/api/v9/guilds/${guild}/messages/search?channel_id=${channel}&min_id=${min_id}&max_id=${max_id}&sort_by=timestamp&sort_order=asc&offset=0`
+	let results
+	try {
+		let response = await fetch(url, {headers: {'authorization': env.auth}})
+		let json = await response.json()
+		results = json.total_results
+	} catch (error) {
+		console.error(error)
+		await sleep(60_000)
+		continue
+	}
+
+	let date_string = d.toLocaleDateString()
+	data[date_string] = results
+	console.info(`${date_string}: ${results}`)
+
+	d = d_next
+	await sleep(2_000)
+}
+
+writeFileSync('./data.json', JSON.stringify(data, undefined, 2))
+console.info('done!')
+
+// https://discord.com/developers/docs/reference#snowflakes
+function date_to_snowflake(date = new Date()) {
+	let DISCORD_EPOCH = new Date('1/1/2015 UTC')
+	let diff = date.getTime() - DISCORD_EPOCH.getTime()
+	let binary = diff.toString(2).padStart(42, '0')
+	let binary_padded = binary + '00000' + '00000' + '000000000000'
+	let snowflake = parseInt(binary_padded, 2)
+	return snowflake
+}
+
+async function sleep(timeout) {
+	return new Promise (function (resolve) {setTimeout(resolve, timeout)})
+}
